c109144d52ae8e71b45644fead64ae1a8d17859d
camenduru <54370274+camenduru@users.noreply.github.com>
2023-11-30 21:27:57 +0300

    fp16=True

--- web_demo_audio.py
+++ web_demo_audio.py
@@ -51,6 +51,7 @@ def _load_model_tokenizer(args):
         trust_remote_code=True,
         low_cpu_mem_usage=True,
         resume_download=True,
+        fp16=True,
         device_map='auto'
     ).eval()
     model.generation_config = GenerationConfig.from_pretrained(
