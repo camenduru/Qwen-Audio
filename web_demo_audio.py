6dffc834f74140bc86f8885f6180e807f06972a0
camenduru <54370274+camenduru@users.noreply.github.com>
2023-11-30 20:13:03 +0300

    load_in_8bit

--- web_demo_audio.py
+++ web_demo_audio.py
@@ -46,21 +46,13 @@ def _load_model_tokenizer(args):
         args.checkpoint_path, trust_remote_code=True, resume_download=True,
     )
 
-    kwargs = {"device_map": "auto"}
-    kwargs['load_in_4bit'] = True
-    kwargs['quantization_config'] = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_compute_dtype=torch.float16,
-        bnb_4bit_use_double_quant=True,
-        bnb_4bit_quant_type='nf4'
-    )
-
     model = AutoModelForCausalLM.from_pretrained(
         args.checkpoint_path,
         trust_remote_code=True,
         low_cpu_mem_usage=True,
         resume_download=True,
-        **kwargs
+        device_map='auto',
+        load_in_8bit=True
     ).eval()
     model.generation_config = GenerationConfig.from_pretrained(
         args.checkpoint_path, trust_remote_code=True, resume_download=True,
